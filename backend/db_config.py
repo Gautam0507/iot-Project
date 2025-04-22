96460567d875a433b2b4bc469f26de38d116dd0f
Gautam <gautam.nraj@gmail.com>
2025-04-22 13:03:23 +0530

    added config

--- backend/db_config.py
+++ backend/db_config.py
@@ -8,8 +8,8 @@ def get_db_config() -> Dict[str, Any]:
     """
     return {
         "host": "localhost",
-        "user": "fastapi_user",
-        "password": "fastApi_Pass456!",
+        "user": "root",
+        "password": "pass",
         "database": "fastapi_db",
         "port": 3306,
     }
